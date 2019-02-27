4bf66ae9317387e7247f5fac4d2f1480eaa2580c
Taylor Cooke <taylorcooke@gmail.com>
2019-02-27 14:26:45 -0500

    created ComPlayer class

--- pig_game.py
+++ pig_game.py
@@ -6,7 +6,7 @@ class HumanPlayer():
     # i think this may be too many attributes for init statement
     def __init__(self, overall_score):
         # self.roll_choice = roll_choice
-        # self.dice_roll = dice_roll
+        # self.die_roll = die_roll
         # self.rolls_this_turn = rolls_this_turn
         # self.current_turn_score = current_turn_score
         self.overall_score = overall_score
@@ -17,20 +17,20 @@ class HumanPlayer():
         self.roll_choice = input("Would you like to roll the dice? (y/n): ")
         return self.roll_choice
 
-    def roll_dice(self, roll_choice):
+    def roll_die(self, roll_choice):
         """User rolls dice"""
         if roll_choice = 'y':   
-            self.dice_roll = random.randint(0,6)
-            print(dice_roll)
+            self.die_roll = random.randint(0,6)
+            print(die_roll)
             self.rolls_this_turn += 1
             print(self.rolls_this_turn)
-            return self.dice_roll
+            return self.die_roll
 
     
-    def current_turn_score(self, dice_roll):
+    def current_turn_score(self, die_roll):
         """Adding dice roll score to current turn score"""
         self.current_turn_score = 0
-        self.current_turn_score += dice_roll
+        self.current_turn_score += die_roll
         return self.current_turn_score
     
     def overall_score(self, runnning_score, current_turn_score):
@@ -39,13 +39,50 @@ class HumanPlayer():
         print(self.overall_score)
         return self.overall_score
 
-class Dice():
+class Die():
+    """Creating the die"""
+
     def __init__(self, sides):
         self.sides = sides
 
-    def roll_dice(self):
-        self.dice_roll = random.randint(0,self.sides)
-        return self.dice_roll
+    def roll_die(self):
+        self.die_roll = random.randint(0,self.sides)
+        return self.die_roll
 
+class ComPlayer():
+    """Creating the computer player for Pig game"""
+
+    # i think this may be too many attributes for init statement
+    def __init__(self, overall_score):
+        # self.roll_choice = roll_choice
+        # self.die_roll = die_roll
+        # self.rolls_this_turn = rolls_this_turn
+        # self.current_turn_score = current_turn_score
+        self.overall_score = overall_score
+        # self.computer_score = computer_score
+
+    def roll_dice(self, roll_choice):
+        """User rolls dice"""
+        if current_turn_score < 20:   
+            self.die_roll = random.randint(0,6)
+            print(die_roll)
+            self.rolls_this_turn += 1
+            print(self.rolls_this_turn)
+            return self.die_roll
+
+    
+    def current_turn_score(self, die_roll):
+        """Adding dice roll score to current turn score"""
+        self.current_turn_score = 0
+        self.current_turn_score += die_roll
+        return self.current_turn_score
+    
+    def overall_score(self, runnning_score, current_turn_score):
+        """Adding turn score to overall score"""
+        self.overall_score += self.current_turn_score
+        print(self.overall_score)
+        return self.overall_score
+
+class Game():
 
 
